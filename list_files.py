3bc30253699f35680d3df3d9eb8fd1410a9cfd33
ann_chang <ann_chang@trendmicro.com>
2023-09-18 02:26:05 +0800

    [feature]add argparse for username, foldername, sort_name, sort_created, asc or desc

---
+++ list_files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Virtual File System - List files")
+
+    parser.add_argument("username", type=str, help="Name of the user")
+    parser.add_argument("foldername", type=str, help="Name of the folder")
+
+    sort_group = parser.add_mutually_exclusive_group(required=True)
+    sort_group.add_argument("--sort_name", action="store_true", help="Sort by file name")
+    sort_group.add_argument("--sort_created", action="store_true", help="Sort by file creation date")
+    parser.add_argument("order", choices=["asc", "desc"], default="asc", help="Sort in ascending or descending")
+
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
