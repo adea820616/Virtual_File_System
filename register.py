7ddcd36b5b52f8553d1f96bc3aba5a19858b8746
ann_chang <ann_chang@trendmicro.com>
2023-09-18 00:14:33 +0800

    [feature]add check the format of username

--- register.py
+++ register.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import argparse
+from utils import check_format
 
 def main():
     parser = argparse.ArgumentParser(description="Virtual File System - Register")
     parser.add_argument("username", type=str, help="register a user")
 
     args = parser.parse_args()
+
+    # check the format of username
+    username_flag = check_format(args.username)
 
 
 if __name__ == "__main__":
