9c975a8addf4cf943e9d35a1dfcf79b100cb7a46
ann_chang <ann_chang@trendmicro.com>
2023-09-18 00:26:46 +0800

    [feature]add the function to check if the username exists or not

--- register.py
+++ register.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 from utils import check_format
+from user_utils import UserManagement
 
 def main():
     parser = argparse.ArgumentParser(description="Virtual File System - Register")
@@ -11,6 +12,10 @@ def main():
     # check the format of username
     username_flag = check_format(args.username)
     username = args.username.lower()
+
+    # check if the user exists or not
+    vfs_user = UserManagement()
+    user_exists_flag = vfs_user.check_exists(username, True)
 
 
 if __name__ == "__main__":
