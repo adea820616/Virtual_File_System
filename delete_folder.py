b1e5604ebf4f585338d26f03923bc71d2452a16b
ann_chang <ann_chang@trendmicro.com>
2023-09-18 00:56:42 +0800

    [feature]add function of checking  the format of username and foldername

--- delete_folder.py
+++ delete_folder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from utils import check_format_pack
 
 def main():
     parser = argparse.ArgumentParser(description="Virtual File System - Delete a folder")
@@ -7,6 +8,9 @@ def main():
     parser.add_argument("foldername", type=str, help="Name of the folder to delete")
 
     args = parser.parse_args()
+
+    # check the format of username & foldername
+    name_is_ok = check_format_pack([args.username, args.foldername])
 
 
 if __name__ == "__main__":
