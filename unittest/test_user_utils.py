c740de932f361ab770872c079f8b4c4add28a44c
ann_chang <ann_chang@trendmicro.com>
2023-09-18 15:16:33 +0800

    [test]add unittest for checking user does not exist

--- unittest/test_user_utils.py
+++ unittest/test_user_utils.py
@@ -16,3 +16,11 @@ class TestUserManagement(unittest.TestCase):
             output = mock_stderr.getvalue().strip()
             self.assertTrue(result)
             self.assertIn(output, "The 'test_user' exists.")
+
+    def test_check_user_does_not_exist(self):
+        "checks the check_exists method when the user does not exist."
+        with patch('sys.stderr', new_callable=StringIO) as mock_stderr:
+            result = self.vfs_user.check_exists('nonexistent_user', False)
+            output = mock_stderr.getvalue().strip()
+            self.assertFalse(result)
+            self.assertIn(output, "The 'nonexistent_user' has not registered.")
