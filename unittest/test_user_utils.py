bd7a3d9a13814ec7f954067d1616b59ce0b817a7
ann_chang <ann_chang@trendmicro.com>
2023-09-18 16:18:45 +0800

    [test] remove init class, adjust it into each function

--- unittest/test_user_utils.py
+++ unittest/test_user_utils.py
@@ -20,12 +20,10 @@ class TestUserManagement(unittest.TestCase):
     Usage:
         python test_user_utils.py
     """
-    def init_class(self):
-        "initializes a UserManagement instance for each test case."
-        self.vfs_user = UserManagement()
 
     def test_check_user_exists(self):
         "checks the check_exists method when the user exists."
+        self.vfs_user = UserManagement()
         with patch('sys.stderr', new_callable=StringIO) as mock_stderr:
             result = self.vfs_user.check_exists('test_user', False)
             output = mock_stderr.getvalue().strip()
@@ -34,6 +32,7 @@ class TestUserManagement(unittest.TestCase):
 
     def test_check_user_does_not_exist(self):
         "checks the check_exists method when the user does not exist."
+        self.vfs_user = UserManagement()
         with patch('sys.stderr', new_callable=StringIO) as mock_stderr:
             result = self.vfs_user.check_exists('nonexistent_user', False)
             output = mock_stderr.getvalue().strip()
@@ -42,6 +41,7 @@ class TestUserManagement(unittest.TestCase):
 
     def test_register_user_success(self):
         "Test the register_user method for successful registration."
+        self.vfs_user = UserManagement()
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             self.vfs_user.register_user('new_user')
             output = mock_stdout.getvalue().strip()
