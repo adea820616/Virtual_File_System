59e87491c077cb791eb1946219a112bbc84dd74b
ann_chang <ann_chang@trendmicro.com>
2023-09-18 15:15:46 +0800

    [test]add unittest for checking user exists

---
+++ unittest/test_user_utils.py
@@ -0,0 +1,18 @@
+import unittest
+from user_utils import UserManagement
+from io import StringIO
+from unittest.mock import patch
+
+class TestUserManagement(unittest.TestCase):
+
+    def init_class(self):
+        "initializes a UserManagement instance for each test case."
+        self.vfs_user = UserManagement()
+
+    def test_check_user_exists(self):
+        "checks the check_exists method when the user exists."
+        with patch('sys.stderr', new_callable=StringIO) as mock_stderr:
+            result = self.vfs_user.check_exists('test_user', False)
+            output = mock_stderr.getvalue().strip()
+            self.assertTrue(result)
+            self.assertIn(output, "The 'test_user' exists.")
