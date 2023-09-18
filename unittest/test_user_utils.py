f235b748c606f75c5735de787d21cb207a79c7b2
ann_chang <ann_chang@trendmicro.com>
2023-09-18 15:19:13 +0800

    [config]add abspath

--- unittest/test_user_utils.py
+++ unittest/test_user_utils.py
@@ -1,4 +1,7 @@
 import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from user_utils import UserManagement
 from io import StringIO
 from unittest.mock import patch
